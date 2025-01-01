62c3e997816be0fc93e57deb61ba4e10479b50b8
alparslan <alparslanorgnl@gmail.com>
2025-01-01 23:37:02 +0300

    slider component modife

--- app/components/SliderComponent.js
+++ app/components/SliderComponent.js
@@ -4,19 +4,22 @@ import { useState } from "react";
 
 export default function SliderComponent({data, practice}) {
    const [selectedImageUrl, setSelectedImageUrl] = useState(null);
-
    const getImageUrl = (item) => practice === 'Flashcards' ? item : item.imagePath;
-  
+ 
    return (
        <div className="flex justify-center w-full">
-           <div className="carousel rounded-box flex md:flex-row flex-col space-y-4 md:space-y-0 md:space-x-4 mt-5 gap-5">
+           <div className="carousel rounded-box flex md:flex-row flex-col space-y-4 md:space-y-0 md:space-x-4 gap-5 h-[550px] overflow-y-hidden items-center px-10">
                {data.map((item) => (
-                   <div 
-                       key={item.id} 
-                       className={`carousel-item flex-shrink-0 cursor-pointer ${
-                           practice === 'Listening' 
-                               ? 'w-[400px] flex flex-col' 
+                   <div
+                       key={item.id}
+                       className={`carousel-item flex-shrink-0 cursor-pointer transform transition-all duration-300 ${
+                           practice === 'Listening'
+                               ? 'w-[400px] flex flex-col'
                                : 'w-[300px] h-[487px] relative'
+                       } ${
+                           selectedImageUrl === getImageUrl(item)
+                           ? 'scale-110 shadow-lg'
+                           : 'hover:scale-102'
                        }`}
                        onClick={() => setSelectedImageUrl(getImageUrl(item))}
                    >
